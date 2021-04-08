e5b85f6148730c5ce59c5ec516f05476c470dff0
vikash2806 <vikashrajendran2806@gmail.com>
2021-04-08 10:46:23 +0530

    router added , bootstrap added

---
+++ src/views/Create.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="create">
+  <h3>This is create vue</h3>
+</div>
+</template>
+
+<script>
+export default {
+    name:"create",
+    components:{
+
+    }
+}
+</script>
+
+<style>
+
+</style>
